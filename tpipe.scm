e71e2d6af5ec408cfcdd7db3145a2c3ec53f27a5
Robert Altenburg <rca@qrpc.com>
2019-07-27 22:09:18 -0400

    added command line options

--- tpipe.scm
+++ tpipe.scm
@@ -8,22 +8,36 @@
 (import (chicken io))
 (import (chicken string))
 (import (chicken port))
+(import (chicken process-context))
+(import args)
 
 ;; execute the tmux command on a line
-(define (pipe-to-tmux line)
-  (let ((command (list (string-append "tmux send-keys -t .+ '" line "\n'"))))
+(define (pipe-to-tmux line target)
+  (let ((command (list (string-append "tmux send-keys -t " target " '" line "\n'"))))
     (execute command)))
 
 ;; read line from stdin
-(define (loop-read in-line)
+(define (loop-read in-line target)
   (unless (eof-object? in-line)
-    (pipe-to-tmux in-line)
-    (loop-read (read-line))))
+    (pipe-to-tmux in-line target)
+    (loop-read (read-line) target)))
 
 ;; could test with (if (terminal-port? (current-input-port))
 ;; and do something different if you are interactive.
 
-(loop-read (read-line))
+(define opts
+  (list (args:make-option (t target) (optional: "TARGET") "target window or pane [default .+]")))
+
+(let* ((arguments (args:parse (command-line-arguments) opts))
+       (t-val (alist-ref 't arguments)))
+  (if t-val
+      (loop-read (read-line) t-val)
+      (loop-read (read-line) ".+")))
+
+
+
+
+;;(loop-read (read-line) ".+")
     
     
 
